09bdcd5f2f3c3568f461c5b1566f70521e47bd7e
BuiKhang201223 <khangbdse173094@fpt.edu.vn>
2023-10-14 15:29:08 +0700

    bug

--- src/main/java/com/huvo2/mathutil/core/MathUtility.java
+++ src/main/java/com/huvo2/mathutil/core/MathUtility.java
@@ -21,7 +21,7 @@ public class MathUtility {
     //ko tính giai thừa âm
     //0! = 1! = 1
     public static long getFactorial(int n){
-        long result = 1;
+        long result = 10;
         
         if (n<0 || n>20) 
             throw new IllegalArgumentException("Invalid n, n must be between 0 and 20");
